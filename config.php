212143fdffc288d99f13bcad5bf545c5e6c4460e
ROBSON DO NASCIMENTO FERREIRA JUNIOR <robsonnfjunior@gmail.com>
2025-09-18 21:11:26 -0300

    config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario-ate';
+
+    $conexao = new mysqli ($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    //if($conexao->connect_errno)
+    //{
+    //    echo "Erro";
+    //}
+
+    //else
+    //{
+    //    echo "CONECTADO COM SUCESSO!!!!";
+    //}
+
+
+?>
